b5635d5aae9b888204f928565f35016d08c54dbc
Martin Griffiths <martin.griffiths@gmail.com>
2019-09-23 22:20:08 +0100

    added get_comments

---
+++ R/get_comments.R
@@ -0,0 +1,71 @@
+#' Get Comments From BGG Game Page
+#'
+#' @param id character vector of BGG game IDs
+#'
+#' @return data frame of comments
+#' @export
+#'
+#' @examples
+#' 
+#' @importFrom rlang .data
+get_comments <- function(id){
+  games_xml <- httr::GET('https://boardgamegeek.com',
+                         path = '/xmlapi2/thing',
+                         query = list('id' = paste(id, collapse = ','),
+                                      'comments' = '1'),
+                         httr::accept_xml())
+  
+  total_comments <- httr::content(games_xml) %>%
+    xml2::xml_find_all('item/comments') %>%
+    xml2::xml_attr('totalitems')
+  
+  pages <- max(ceiling(as.numeric(total_comments)/100))
+  
+  purrr::map_df(1:pages, ~get_comments_page(id, .))
+}
+
+#' Get Comments From BGG Game Page
+#'
+#' @param id character vector of BGG game IDs
+#' @param page page number
+#' @return data frame of comments
+#' @export
+#'
+#' @examples
+#' 
+#' @importFrom rlang .data
+get_comments_page <- function(id, page){
+  Sys.sleep(2)
+  games_xml <- httr::GET('https://boardgamegeek.com',
+                         path = '/xmlapi2/thing',
+                         query = list('id' = paste(id, collapse = ','),
+                                      'comments' = '1',
+                                      'page' = page),
+                         httr::accept_xml())
+  
+  items <- httr::content(games_xml) %>%
+    xml2::xml_find_all('item/comments') %>%
+    purrr::set_names(id)
+  
+  parse_item <- function(item){
+    
+    comments <- item %>%
+      xml2::xml_find_all('comment') 
+    
+    if(length(comments) == 0) {
+      return(NULL)
+    } else {
+    
+      comments_df <- comments %>%
+        xml2::xml_attrs() %>%
+        purrr::map_df(dplyr::bind_rows) %>%
+        dplyr::mutate(rating = as.numeric(rating))
+    
+      return(comments_df)
+    }
+  }
+  
+  items %>%
+    purrr::map_df(parse_item, .id = 'id')
+
+}
