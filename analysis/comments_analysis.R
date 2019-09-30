8ab32557eaeb42c8e648240709c9c174da14a539
Martin Griffiths <martin.griffiths@gmail.com>
2019-09-30 07:47:37 +0100

    analysis

--- analysis/comments_analysis.R
+++ analysis/comments_analysis.R
@@ -1,3 +1,7 @@
+library(dplyr)
+library(readr)
+library(stringr)
+
 params <- list(
   'sort' = 'numvoters',
   'nosubtypes[0]' = 'boardgameexpansion')
@@ -16,12 +20,15 @@ top1000_comments <- dplyr::bind_rows(top100_comments, top200comments, top300comm
 top5000_comments %>%
   readr::write_csv('top5000_comments.csv')
 
-top5000_comments %>%
-  dplyr::mutate(has_term = stringr::str_detect(comment, regex("\\btigris\\b", ignore_case = T))) %>%
-  dplyr::group_by(id, name) %>%
-  dplyr::summarise(n_term = sum(has_term), pc_term = 100*n_term/n()) %>%
-  dplyr::arrange(desc(pc_term))
+top5000_comments <- read_csv('top5000_comments.csv') %>%
+  mutate(id = as.character(id), rating = as.numeric(rating))
 
+top5000_comments %>%
+  filter(str_count(comment, " ") > 10) %>%
+  dplyr::mutate(has_term = stringr::str_detect(comment, regex("\\btrick.taking\\b", ignore_case = T))) %>%
+  dplyr::group_by(id, name) %>%
+  dplyr::summarise(n_term = sum(has_term, na.rm = T), pc_term = 100*n_term/n()) %>%
+  dplyr::arrange(desc(pc_term))
 
 top5000_comments %>%
   dplyr::count(username, sort = T)
@@ -53,3 +60,100 @@ top5000_comments %>%
   dplyr::filter(n() > 50) %>%
   dplyr::summarise(av_rating = mean(rating, na.rm = T)) %>%
   dplyr::arrange(desc(av_rating))
+
+# data subset
+comments_subset <- top5000_comments %>%
+  filter(str_count(comment, " +") > 10) %>%
+  group_by(id) %>%
+  filter(n() >= 100) %>%
+  ungroup()
+  
+# functionalise search
+search_term <- function(data, term){
+  data %>%
+    dplyr::mutate(has_term = stringr::str_detect(comment, regex(paste0("\\b",term), ignore_case = T))) %>%
+    dplyr::group_by(id, name) %>%
+    dplyr::summarise(n_term = sum(has_term, na.rm = T), pc_term = 100*n_term/n()) %>%
+    dplyr::arrange(desc(pc_term))
+}
+
+comments_subset %>%
+  search_term("abstract")
+
+# iterate to create big term df
+terms <- list("abstract" = "abstract",
+              "wargame" = "war.?game",
+              "ameritrash" = "ameritrash",
+              "euro" = "euro\\b",
+              "party" = "party",
+              "traditional" = "traditional",
+              "bluffing" = "bluffing",
+              "deduction" = "deduction",
+              "dexterity" = "dexterity",
+              "negotiation" = "negotiation",
+              "socialdeduction" = "social.?deduction",
+              "speed" = "speed.?game",
+              "trivia" = "trivia\\b",
+              "word" = "word.?game",
+              "areamajority" = "area.?majority",
+              "deckbuilding" = "deck.?building",
+              "drafting" = "drafting",
+              "enginebuilding" = "engine",
+              "investment" = "investment",
+              "network" = "network",
+              "pickup" = "pick.?up",
+              "workerplacement" = "worker.?placement",
+              "rummy" = "rummy",
+              "memory" = "memory",
+              "patternrecognition" = "pattern.?recognition",
+              "pushyourluck" = "push.?your.?luck",
+              "setcollection" = "set.?collection",
+              "racing" = "racing",
+              "tilelaying" = "tile.?laying",
+              "trading" = "trading",
+              "tricktaking" = "trick.?taking",
+              "rpg" = "rpg",
+              "dudesonamap" = "dudes.?on.?a.?map|doam",
+              "hexandcounter" = "hex.+counter",
+              "simulation" = "simulation",
+              "specialpower" = "special.?power",
+              "takethat" = "take.?that",
+              "auction" = "auction.?game",
+              "civ" = "civ.?game",
+              "coop" = "co.?op\\b",
+              "partnership" = "partnership", 
+              "solo" = "solo\\b",
+              "traitor" = "traitor"
+              )
+
+term_df <- purrr::map_df(terms, ~search_term(comments_subset, .), .id = 'term')
+
+# scale
+term_df <- term_df %>%
+  group_by(term) %>%
+  mutate(pc_term = pc_term/max(pc_term))
+
+term_df_bin <- term_df %>%
+  mutate(pc_term = ifelse(n_term > 5, pc_term, 0))
+
+# summarise by cosine similarity
+
+find_sim <- function(data, game_id){
+  probe <- data %>%
+    filter(id == game_id) %>% 
+    pull(pc_term)
+  
+  data %>%
+    group_by(id, name) %>%
+    summarise(sim = lsa::cosine(pc_term, probe)) %>%
+    arrange(desc(sim))
+}
+
+find_sim(term_df_bin, "42") %>% View()
+
+term_df %>% 
+  group_by(id, name) %>% 
+  filter(pc_term > 0.1) %>% 
+  filter(n() > 4) %>% 
+  arrange(name) %>% View()
+
